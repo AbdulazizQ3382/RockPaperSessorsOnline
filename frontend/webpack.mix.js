108937955f9084ffd34c6322bba1afea768c4ad9
AbdulazizQ3382 <abuqannam2@gmail.com>
2022-07-11 16:33:11 +0300

    create play btns ui

--- frontend/webpack.mix.js
+++ frontend/webpack.mix.js
@@ -2,5 +2,8 @@ let mix = require('laravel-mix');
 require('laravel-mix-ejs')
 require('laravel-mix-modernizr');
 mix.ts('src/app.ts', 'dist')
-    .sass('assets/scss/style.scss', 'dist/public/css')
-    .ejs('components/index.ejs','dist/public').modernizr();
+    .sass('public/scss/style.scss', 'dist/public/css')
+    .ejs('public/index.ejs','dist/public').modernizr();
+
+mix.copy('public/icons', 'dist/public/icons')
+    .copy('public/fonts','dist/public/fonts');
